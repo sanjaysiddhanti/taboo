7cf221cb67748de1a04cf0bef7cfaeeadec2b16b
Sanjay Siddhanti <sanjay.siddhanti@gmail.com>
2020-03-24 23:35:32 -0700

    Add DB

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ build: .
 	docker build -t taboo:latest .
 
 shell: build
-	docker run --rm -it taboo:latest /bin/bash
+	docker run -v $(PWD):/app --rm -it --link postgres:postgres taboo:latest /bin/bash
 
 lint: build
-	docker run --rm taboo:latest black --check --diff
+	docker run --rm taboo:latest black --check --diff /app
 
 format: build
-	docker run -v $PWD:/app --rm taboo:latest black
+	docker run -v $(PWD):/app --rm taboo:latest black /app/
 
 run: build
-	docker run --rm -p 5000:5000 -e "FLASK_APP=src/app.py" \
+	docker run --rm -p 5000:5000 -e "FLASK_APP=src/app.py" --link postgres:postgres \
 	taboo:latest flask run --port 5000 --host 0.0.0.0
+
+db:
+	docker run -e POSTGRES_PASSWORD=postgres -d -p 5400:5432 --name postgres postgres
--- src/app.py
+++ src/app.py
@@ -1,6 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@postgres:5400"
+db = SQLAlchemy(app)
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(14), nullable=False)
+    game_id = db.Column(db.Integer, db.ForeignKey("game.id"), nullable=False)
+    game = db.relationship("Game", backref=db.backref("players", lazy=True))
+
+    def __repr__(self):
+        return f"<Player {self.name} in game {self.game_id}>"
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.String(4), unique=True)
+
+    def __repr__(self):
+        return f"<Game {self.id}>"
+
+
+def generate_room_id():
+    while True:
+        room_id = "".join(random.choice(string.ascii_uppercase) for k in range(4))
+        # Make sure another game doesn't have this room ID
+        if not Game.query.filter(Game.room_id == room_id).first():
+            return room_id
