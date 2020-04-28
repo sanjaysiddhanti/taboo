5d59ac7f71bed40c43f8a252e3177054e56377a9
Sanjay Siddhanti <sanjay.siddhanti@gmail.com>
2020-04-28 00:09:04 -0700

    Use jsonify

--- src/app.py
+++ src/app.py
@@ -20,8 +20,8 @@ class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), unique=True)
 
-    def to_json(self):
-        return json.dumps({"id": self.id, "name": self.name})
+    def to_dict(self):
+        return {"id": self.id, "name": self.name}
 
 
 class Prompt(db.Model):
@@ -29,14 +29,13 @@ class Prompt(db.Model):
     target_word = db.Column(db.String(25), unique=True)
     banned_words = db.Column(JSONB)
 
-    def to_json(self):
-        return json.dumps(
-            {
-                "id": self.id,
-                "target_word": self.target_word,
-                "banned_words": self.banned_words,
-            }
-        )
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "target_word": self.target_word,
+            "banned_words": self.banned_words,
+        }
+        
 
 
 class GamePrompt(db.Model):
@@ -48,6 +47,13 @@ class GamePrompt(db.Model):
     game = relationship("Game", uselist=False)
     prompt = relationship("Prompt", uselist=False)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "prompt_id": self.prompt_id,
+            "game_id": self.game_id,
+            "used": self.used,
+        }
 
 class NameAlreadyTaken(Exception):
     status_code = 500
@@ -90,24 +96,29 @@ def create_game():
         game_prompt = GamePrompt(prompt_id=prompt.id, game_id=new_game.id, used=False)
         db.session.add(game_prompt)
     db.session.commit()
-    return new_game.to_json()
+    return jsonify(new_game.to_dict())
 
 
 @app.route("/game/<game_name>", methods=["GET"])
 def get_game(game_name: str):
     game = Game.query.filter_by(name=game_name).first()
     if game:
-        return game.to_json()
+        return jsonify(game.to_dict())
 
 
 @app.route("/game/<game_name>/prompts", methods=["GET"])
 def get_game_prompts(game_name: str):
     """Get the unused prompts for a game
     """
-    game = GamePrompt.query.filter_by(name=game_name).first()
+    game = Game.query.filter_by(name=game_name).first()
     if game:
-        prompts = db.session.query(GamePrompt).join(GamePrompt.game, GamePrompt.prompt).filter(Game.name==game_name, GamePrompt.used==False).paginate()
-        return jsonify(prompts)
+        prompts = (
+            db.session.query(GamePrompt)
+            .join(GamePrompt.game, GamePrompt.prompt)
+            .filter(Game.name == game_name, GamePrompt.used == False)
+            .paginate()
+        )
+        return jsonify([game_prompt.to_dict() for game_prompt in prompts.items])
 
 
 @app.errorhandler(NameAlreadyTaken)
